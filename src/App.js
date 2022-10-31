64e04078e5192109c8ce05ef30115b3873cda4ec
Alexander Gozali <alexandergozali0@gmail.com>
2022-10-31 17:31:00 +0700

    Feat: Database and Fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(response => response.json())
+      .then(json => {
+        // Get the random solution from the database.
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        
+        // Set the new solution value with the value of "word" key.
+        setSolution(randomSolution.word);
+      })
+  }, [setSolution]); // useEffect called again if there are changes in setSolution
+
   return (
     <div className="App">
       <h1>Wordle</h1>
+
+      {
+        solution
+          && <div>Solution is: {solution}</div>
+      }
     </div>
   );
 }
